df0389c60c90f5879dbd944c0657db4e5a071619
= <yashas1145@proton.me>
2026-02-14 13:52:54 +0530

    add fetch env variables functionality

--- database/database.go
+++ database/database.go
@@ -3,6 +3,9 @@ package database
 import (
 	"database/sql"
 	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
 var DB *sql.DB
@@ -10,7 +13,18 @@ var DB *sql.DB
 func init() {
 	var err error
 
-	connString := fmt.Sprintf("host=%v user=%v password=%v port=%v dbname=%v sslmode=disable", "localhost", "postgres", "kjm40438", 5432, "codesnippetApi")
+	err = godotenv.Load(".env")
+	if err != nil {
+		panic("error loading environment variables")
+	}
+
+	var DBHOST = os.Getenv("DB_HOST")
+	var DBUSER = os.Getenv("DB_USER")
+	var DBPASS = os.Getenv("DB_PASS")
+	var DBNAME = os.Getenv("DB_NAME")
+	var DBPORT = os.Getenv("DB_PORT")
+
+	connString := fmt.Sprintf("host=%v user=%v password=%v port=%v dbname=%v sslmode=disable", DBHOST, DBUSER, DBPASS, DBPORT, DBNAME)
 
 	DB, err = sql.Open("postgres", connString)
 	if err != nil {
